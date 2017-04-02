a7c877249fa12c7fb712988b31cdd6440a86b874
Josef Adlao <jadlao@gmail.com>
2017-04-03 07:39:42 +1000

    added smooth scrolling and jquery

--- js/olooq.js
+++ js/olooq.js
@@ -1,1 +1,11 @@
 // Main js script file for website pages (not landing page)
+// Smooth scroll to any anchor specified
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
