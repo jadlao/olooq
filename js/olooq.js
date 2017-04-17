a220731574de05e3ddbcbc787b287134f2ced451
Josef Adlao <jadlao@gmail.com>
2017-04-17 13:56:05 +1000

    added carousel

--- js/olooq.js
+++ js/olooq.js
@@ -8,35 +8,3 @@ $('a[href^="#"]').on('click', function(event) {
         }, 1000);
     }
 });
-
-// Carousel code 
-$(document).ready(function(){
-    $('#carousel').slick({
-        centerMode: true,
-        centerPadding: '10px',
-        slidesToShow: 3,
-        autoplay: true,
-        arrows: true,
-        dots: true,
-        responsive: [
-            {
-            breakpoint: 768,
-            settings: {
-                arrows: true,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-              }
-            },
-            {
-            breakpoint: 480,
-            settings: {
-                arrows: true,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-              }
-            }
-      ]
-    });
-});
